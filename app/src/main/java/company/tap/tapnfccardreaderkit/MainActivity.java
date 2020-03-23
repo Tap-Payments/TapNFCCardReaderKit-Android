3a13b8c3b72f6172b37d61fa3a6b1e240f2c8044
Mario Gamal <m.gamal@tap.company>
2020-03-23 13:06:50 +0300

    Initialize Repository

---
+++ app/src/main/java/company/tap/tapnfccardreaderkit/MainActivity.java
@@ -0,0 +1,14 @@
+package company.tap.tapnfccardreaderkit;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
